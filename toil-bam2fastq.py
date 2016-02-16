574adbcd0df54e33ac90b4f587e2417f8850b969
jpdna <justinpaschalldna@gmail.com>
2016-02-16 09:21:16 -0500

    change toil-bam2fastq.py to output two paired fastq files

--- toil-bam2fastq.py
+++ toil-bam2fastq.py
@@ -45,7 +45,9 @@ def runBAM2FASTQ(job, input_args, mystring):
     src_bucket = input_args['src_bucket']
     dest_bucket = input_args['dest_bucket']
 
-    myfile_no_bucket = mystring.replace("s3://cgl-sgdd-reorg/", "")
+    #myfile_no_bucket = mystring.replace("s3://cgl-sgdd-reorg/", "")
+    
+    myfile_no_bucket = mystring.replace("s3://" + src_bucket + "/", "")
     mydir_no_bucket = os.path.dirname('/' + myfile_no_bucket)
 
     s3 = boto.connect_s3()
@@ -64,18 +66,32 @@ def runBAM2FASTQ(job, input_args, mystring):
     parameters = ['bam2fq', '/data/mybam.bam']
 
     base_docker_call = 'docker run -dit --log-driver=none --entrypoint=/bin/bash -v {}:/data'.format(work_dir).split() 
-    base_docker_call = base_docker_call + ['quay.io/ucsc_cgl/samtools:1.2--35ac87df5b21a8e8e8d159f26864ac1e1db8cf86'] 
+    base_docker_call = base_docker_call + ['quay.io/ucsc_cgl/samtools:1.3--323f40b0c5dace98ced5521c6ae45a6c04df923b'] 
     containerName = subprocess.check_output(base_docker_call)
     formatcontainerName = containerName.rstrip()
     
     # would prefer to use subprocess_check_call() rather than os.system()
     # but had trouble escaping the nested quotes when using shell=True with subprocess
-    mycommand = "docker exec " + formatcontainerName + " sh -c 'samtools bam2fq /data/mybam.bam > /data/output_fastq.fastq'"
+    #mycommand = "docker exec " + formatcontainerName + " sh -c 'samtools bam2fq /data/mybam.bam > /data/output_fastq.fastq'"
+    
+    mycommand = "docker exec " + formatcontainerName + " sh -c 'samtools fastq -1 /data/output_1.fastq -2 /data/output_2.fastq -0 /data/output_0.fastq /data/mybam.bam'"
+    
+
+
     os.system(mycommand)
 
+
+
     # Docker writes file with rw permission only by root at moment, fixed by chmod so toil can delete it when done
-    mycommand2 = "docker exec " + formatcontainerName + " sh -c 'chmod 666 /data/output_fastq.fastq'"
+    mycommand2 = "docker exec " + formatcontainerName + " sh -c 'chmod 666 /data/output_1.fastq'"
     os.system(mycommand2)
+
+    mycommand2 = "docker exec " + formatcontainerName + " sh -c 'chmod 666 /data/output_2.fastq'"
+    os.system(mycommand2)
+
+    mycommand2 = "docker exec " + formatcontainerName + " sh -c 'chmod 666 /data/output_0.fastq'"
+    os.system(mycommand2)
+
 
     myclosecommand = "docker stop " + formatcontainerName
     os.system(myclosecommand)
@@ -84,18 +100,82 @@ def runBAM2FASTQ(job, input_args, mystring):
     myrm_command = "rm " + os.path.join(work_dir, 'mybam.bam')
     os.system(myrm_command)
 
+    
+    myfilename = myfilename.replace(".raw.bam", "")
+    myfilename = myfilename.replace(".bam", "")
+
     #zip the fastq file
     #pigz must be available on worker machine, gzip could be used instead, but slower
     # todo:make a pigz docker image available on quay.io to remove dependency
-    myzip_command = "pigz -c -p 30 " + os.path.join(work_dir, 'output_fastq.fastq') + " > " + os.path.join(work_dir, myfilename +  ".fastq.gz")
+    myzip_command = "pigz -c -p 30 " + os.path.join(work_dir, 'output_1.fastq') + " > " + os.path.join(work_dir, myfilename +  "_1.fastq.gz")
     os.system(myzip_command)
 
+    myzip_command = "pigz -c -p 30 " + os.path.join(work_dir, 'output_2.fastq') + " > " + os.path.join(work_dir, myfilename +  "_2.fastq.gz")
+    os.system(myzip_command)
+
+    myzip_command = "pigz -c -p 30 " + os.path.join(work_dir, 'output_0.fastq') + " > " + os.path.join(work_dir, myfilename +  "_0.fastq.gz")
+    os.system(myzip_command)
+
+
     #Upload to S3         
-    s3am_command1= ['s3am', 'cancel', 's3://cgl-sgdd-fastq' + mydir_no_bucket + '/' + myfilename + '.fastq.gz']
+
+    s3am_command1= ['s3am', 'cancel', 's3://' + dest_bucket + '/sequence/' + myfilename + '_1.fastq.gz']
 
     s3am_command2 = ['s3am', 'upload', '--upload-slots=20', '--download-slots=20', 
-                    '--part-size=50000000', 'file://{}'.format(os.path.join(work_dir, myfilename + '.fastq.gz')), 
-                    's3://' + dest_bucket + mydir_no_bucket + '/' + myfilename + '.fastq.gz']
+                    '--part-size=50000000', 'file://{}'.format(os.path.join(work_dir, myfilename + '_1.fastq.gz')), 
+                    's3://' + dest_bucket + '/sequence/' + myfilename + '_1.fastq.gz']
+
+
+    #Retry loop as s3am calls failed sporadically 
+    retries = 20
+    for i in range(retries):
+      try:
+        subprocess.check_call(s3am_command1)
+        time.sleep(5)
+        subprocess.check_call(s3am_command2)
+      except:
+          if i < retries:
+            time.sleep(5)
+            continue
+          else:
+            raise
+      break
+    
+
+
+
+    s3am_command1= ['s3am', 'cancel', 's3://' + dest_bucket + '/sequence/' + myfilename + '_2.fastq.gz']
+
+    s3am_command2 = ['s3am', 'upload', '--upload-slots=20', '--download-slots=20', 
+                    '--part-size=50000000', 'file://{}'.format(os.path.join(work_dir, myfilename + '_2.fastq.gz')), 
+                    's3://' + dest_bucket + '/sequence/' + myfilename + '_2.fastq.gz']
+
+
+    #Retry loop as s3am calls failed sporadically 
+    retries = 20
+    for i in range(retries):
+      try:
+        subprocess.check_call(s3am_command1)
+        time.sleep(5)
+        subprocess.check_call(s3am_command2)
+      except:
+          if i < retries:
+            time.sleep(5)
+            continue
+          else:
+            raise
+      break
+    
+
+
+    s3am_command1= ['s3am', 'cancel', 's3://' + dest_bucket + '/sequence/' + myfilename + '_0.fastq.gz']
+
+    s3am_command2 = ['s3am', 'upload', '--upload-slots=20', '--download-slots=20', 
+                    '--part-size=50000000', 'file://{}'.format(os.path.join(work_dir, myfilename + '_0.fastq.gz')), 
+                    's3://' + dest_bucket + '/sequence/nopair/' + myfilename + '_0.fastq.gz']
+
+
+
 
     #Retry loop as s3am calls failed sporadically 
     retries = 20
@@ -111,6 +191,8 @@ def runBAM2FASTQ(job, input_args, mystring):
           else:
             raise
       break  
+
+
     return 
 
 def main():
@@ -142,4 +224,5 @@ if __name__ == '__main__':
     main()
 
 #example usage
-#python toil-bam2fastq.py aws:us-west-2:jp-test-208 --file_list filelist.txt  --src_bucket='cgl-sgdd-reorg' --dest_bucket='cgl-sgdd-fastq' --batchSystem=mesos --mesosMaster=$(hostname -i):5050 --retryCount 5  --workDir /var/lib/toil/ > jptoil_run.log 2>&1 &
+#python toil-bam2fastq.py aws:us-west-2:jp-test-700 --file_list filelist.txt  --src_bucket='cgl-sgdd-reorg' --dest_bucket='cgl-sgdd-work' --batchSystem=mesos --mesosMaster=$(hostname -i):5050 --retryCount 5  --workDir /var/lib/toil/ > jptoil_run.log 2>&1 &
+
